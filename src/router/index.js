e8ed4c9e401160fdd0f11f8884d9dc7c10cc60a3
csy9730 <csy9730@users.noreply.github.com>
2023-05-20 17:23:06 +0800

    add todo
    add charts

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NotFound from '@/views/NotFound'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    component: () => import('../views/DemoView.vue')
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    component: () => import('../views/LineView.vue')
+  },
+  {
+    path: '/todo',
+    name: 'todos',
+    component: () => import('../views/TodoView.vue'),
+    children: [{
+      path: '/todo/:id',
+      name: 'todo',
+      component: () => import('../components/Todo/TodoItem.vue')
+    }]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  mode:  'hash', // 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/store/getters.js
@@ -0,0 +1,4 @@
+
+export const gTodoList = state => {
+    return state.todoList; // 派生状态todoList
+};
